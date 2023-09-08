f055e36bd1bdfbd9e1f52ac423580b47a260bc6f
Gui-rosales <guilhermelexandre456@gmail.com>
2023-09-07 23:02:47 -0400

    fix: fixing text on about page

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -11,8 +11,11 @@ export function About() {
         <div className="w-3 h-3 rounded-[50%] bg-primary max-[560px]:w-2 max-[560px]:h-2"></div>
         <div className="w-3 h-3 rounded-[50%] bg-primary max-[560px]:w-2 max-[560px]:h-2"></div>
       </div>
-      <div className="flex w-full h-[55vh] justify-around py-5 px-10 max-[560px]:py-3 max-[560px]:px-5 max-[560px]:justify-between max-[560px]:gap-1">
-        <div className="h-full w-4/12 flex flex-col justify-around text-white max-[560px]:w-1/2">
+      <div className="flex w-full h-[55vh] justify-around py-5 px-10 max-[560px]:py-3 max-[560px]:px-5 max-[560px]:justify-between max-[560px]:gap-1 max-[560px]:h-[80vh] max-[560px]:flex-col">
+        <div className="h-100% w-4/12 flex items-center justify-center max-[560px]:w-1/2">
+          <BoyListening />
+        </div>
+        <div className="h-full w-4/12 flex flex-col text-white max-[560px]:w-full gap-6">
           <h1 className="text-tertiary text-4xl max-[560px]:text-base">
             Quem eu sou
           </h1>
@@ -21,14 +24,14 @@ export function About() {
             possibilidades
           </p>
           <p className="max-[560px]:text-sm">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptates
-            assumenda nesciunt eum quae non sed iste alias, dicta qui voluptatem
-            sit sapiente repudiandae voluptas libero dolorem molestias ut velit.
-            Veritatis!
+            Hoje em dia atuando como desenvolvedor Web sou um jovem que está em
+            processo de formação e aperfeiçoamento dentro da área de
+            desenvolvimento. Sempre preocupado com a entrega de soluções bem
+            escritas e estruturadas, sou um desenvolvedor que sempre busca a
+            milha extra. Atualmente sou graduante em Sistemas de informação pelo
+            Instituto Federal de Mato Grosso e atuo como estagiário em
+            desenvolvimento de software
           </p>
-        </div>
-        <div className="h-100% w-4/12 items-center justify-center max-[560px]:w-1/2">
-          <BoyListening />
         </div>
       </div>
     </section>
